88218b168477636aeb13a59dc1f44fa0baa8a9fc
Pawel  Jelinski <jelinski89@gmail.com>
2017-10-03 22:05:56 +0200

    started implentation of stack kata in F#

---
+++ Stack/FsharpStack/FsharpStack.Tests/FsharpStackTests.fs
@@ -0,0 +1,24 @@
+﻿module MyStackTests 
+ 
+    open Xunit
+    
+    module MyStack =
+        type MyStack = MyStack of int array
+        
+        let create length = 
+            (length, 0) ||> Array.create |> MyStack
+            
+        let length (stack: MyStack) = 
+            stack
+            |> fun (MyStack s) -> s
+            |> Array.length<int>
+    
+    [<Fact>]
+    let ``Given MyStack with length 1 Length should return 1``() =
+        let result = 
+            1 
+            |> MyStack.create
+            |> MyStack.length
+        
+        Assert.Equal(1, result)
+        
