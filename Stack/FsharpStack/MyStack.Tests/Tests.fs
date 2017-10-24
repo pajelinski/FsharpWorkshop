6d6586098ba0bd41b5173affdac4f84d2fd13cbe
Pawel  Jelinski <jelinski89@gmail.com>
2017-10-24 22:42:38 +0200

    started implemntaion of declarative stac

--- Stack/FsharpStack/MyStack.Tests/Tests.fs
+++ Stack/FsharpStack/MyStack.Tests/Tests.fs
@@ -1,8 +1,42 @@
-module Tests
+module MyStack =
 
-open System
-open Xunit
+    type MyStack = MyStack of (int array * int)
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+    let create capacity = 
+        (capacity |> Array.zeroCreate, 0) |> MyStack
+
+    let push item stack =
+        let (MyStack (stackArray, index)) = stack
+        (index, item) ||>  Array.set stackArray
+        (stackArray, index + 1) |> MyStack
+
+    let pop stack = 
+        let (MyStack (stackArray, index)) = stack
+        (index - 1 |> Array.get stackArray, (stackArray, index - 1) |> MyStack)
+
+module Tests =
+
+    open Xunit
+
+    [<Fact>]
+    let ``When pushed 1 It should pop 1`` () =
+        let result = 
+            1
+            |> MyStack.create
+            |> MyStack.push 1
+            |> MyStack.pop
+
+        Assert.Equal(1, result |> fst)
+
+    [<Fact>]
+    let ``When pushed 1 and 2 It should pop 2 and 1`` () =
+        let (firstPop, secondPop) = 
+            2
+            |>MyStack.create
+            |> MyStack.push 1
+            |> MyStack.push 2 
+            |> MyStack.pop 
+            |> fun (popedValue, stack) -> (popedValue, stack |> MyStack.pop |> fst)
+
+        Assert.Equal(2, firstPop)
+        Assert.Equal(1, secondPop)
